6770b37a88068375406854f332c4a100d63ee6c4
suplelive-crm <ph@suplelive.com.br>
2025-06-12 19:16:16 -0300

    Updated tracking-api.ts

--- src/lib/tracking-api.ts
+++ src/lib/tracking-api.ts
@@ -9,6 +9,7 @@ export async function trackCorreios(trackingCode: string): Promise<any> {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        "Authorization": "Apikey WNgBGbjeRSefHGihDVlxlEy3ZHW2EE9z-GtOjW2W684"
       },
       body: JSON.stringify({ "code":trackingCode 
       })
