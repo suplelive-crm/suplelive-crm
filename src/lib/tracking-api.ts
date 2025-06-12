f45d8389a3878e987709d9337fb1b431fbf69c46
suplelive-crm <ph@suplelive.com.br>
2025-06-12 19:41:18 -0300

    Updated tracking-api.ts

--- src/lib/tracking-api.ts
+++ src/lib/tracking-api.ts
@@ -14,7 +14,7 @@ export async function trackPackage(carrier: string, trackingCode: string): Promi
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        // Nenhuma chave de API ou autorização é enviada aqui. Isso é trabalho do back-end!
+        "Authorization": `Apikey WNgBGbjeRSefHGihDVlxlEy3ZHW2EE9z-GtOjW2W684`
       },
       // Enviamos um payload padronizado que nosso back-end entende.
       body: JSON.stringify({ 
