39101340af6247974311556e48702af36b346eab
suplelive-crm <ph@suplelive.com.br>
2025-07-04 17:35:16 -0300

    Updated CreatePurchaseDialog.tsx

--- src/components/tracking/CreatePurchaseDialog.tsx
+++ src/components/tracking/CreatePurchaseDialog.tsx
@@ -112,7 +112,7 @@ export function CreatePurchaseDialog({ open, onOpenChange }: CreatePurchaseDialo
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-2xl max-h-[90vh] overflow-y-auto">
+      <DialogContent className="max-w-4xl max-h-[90vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle>Nova Compra</DialogTitle>
           <DialogDescription>
